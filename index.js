ed444c4077ee15e883ea0a4b96f9d5c4e4c59c6e
greatermeans <gakouri@gmail.com>
2016-07-25 14:16:15 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback) {
+	setTimeout(callback,2000)
+}
+
+function createMultiplier(multiplier) {
+		return function multiplierValue(value) {
+			return (multiplier * value)
+	}
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function multiplier(x,y) {
+	return (x * y)
+}
+
+var doublerWithBind = multiplier.bind(null,2)
+var triplerWithBind = multiplier.bind(null,3)
